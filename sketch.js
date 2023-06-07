b65c71428da38e894e9009b8696c2ac57f5171d8
dejandrea <dejandrea@gmail.com>
2023-06-07 20:08:17 -0300

    lançando a fruta

--- sketch.js
+++ sketch.js
@@ -2,54 +2,55 @@ const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Constraint = Matter.Constraint;
+const Body = Matter.Body;
 
 var engine, world;
-var holder,ball,ground;
-var stand1,stand2;
-var ball;
+var holder, ball, ground;
+var stand1, stand2;
 var slingShot;
 var fruit;
-function preload(){
+var backgroundImg;
+var g;
+
+function preload() {
   backgroundImg = loadImage("background.png");
- fruit=loadImage("melon.png");
- g=loadImage("basket.png")
+  fruit = loadImage("melon.png");
+  g = loadImage("basket.png");
 }
+
 function setup() {
-  createCanvas(900,400);
+  createCanvas(900, 400);
   engine = Engine.create();
   world = engine.world;
   Engine.run(engine);
   ground = new Ground();
-  
- 
 
   //Desafio1:
-  ball = Bodies.(50,200,20);
-  World.add(world,ball);
-//Desafio2
-  slingShot = new Slingshot();
-
+  ball = Bodies.circle(50, 200, 20);
+  World.add(world, ball);
+  //Desafio2
+  slingShot = new Slingshot(ball, { x: 100, y: 100 });
 }
 function draw() {
-  background(backgroundImg); 
- 
+  background(backgroundImg);
+
   //Engine.update(engine);
   //text(mouseX + ',' + mouseY, 10, 15);
-  
+
   ground.display();
-  g.scale=.025;
+  g.scale = 0.025;
 
-
-  imageMode(CENTER)
-  image(fruit ,ball.position.x,ball.position.y,40,40);
-  image(g,450,270)
+  imageMode(CENTER);
+  image(fruit, ball.position.x, ball.position.y, 40, 40);
+  image(g, 450, 270);
 
   slingShot.display();
 }
-function mouseDragged(){
-  Matter.Body.setPosition();
-}
-function mouseReleased(){
-  slingShot.fly();
 
+function mouseDragged() {
+  Matter.Body.setPosition(this.ball,{x:mouseX,y:mouseY});
+}
+
+function mouseReleased() {
+  slingShot.fly();
 }
